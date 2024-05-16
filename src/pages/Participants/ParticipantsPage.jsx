12d1af6fc209f78797ec189307bc27ca62e73067
Natali258 <141056126+Natali258@users.noreply.github.com>
2024-05-16 18:59:17 +0000

    Deploying to main from @ Natali258/events-registration-app@54c202bdc56010924d1ed9de21e5fb7d8bf2270f 🚀

--- src/pages/Participants/ParticipantsPage.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const ParticipantsPage = () => {
-  return (
-    <div>ParticipantsPage</div>
-  )
-}
--- src/services/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export const api = axios.create({
-  baseURL: 'https://66435fd76c6a65658706d34f.mockapi.io/events',
-});
-
-export const setToken = token => {
-  api.defaults.headers.common.Authorization = `Bearer ${token}`;
-};
